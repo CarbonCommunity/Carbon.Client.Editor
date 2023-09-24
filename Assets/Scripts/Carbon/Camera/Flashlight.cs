a44cace801b40a7efcad5a15b1871dcf92b0445a
raul <raulssorban@gmail.com>
2023-09-24 04:10:00 +0200

    Added flashlight (F)

---
+++ Assets/Scripts/Carbon/Camera/Flashlight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Carbon
+{
+	public class Flashlight : MonoBehaviour
+	{
+		public GameObject LightObject;
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.F))
+			{
+				LightObject.SetActive(!LightObject.activeInHierarchy);
+			}
+		}
+	}
+}
