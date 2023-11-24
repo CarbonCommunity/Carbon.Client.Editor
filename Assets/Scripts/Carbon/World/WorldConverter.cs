ee6bffa509b24810d3eea105bc80b065e03c45db
TravisDevLife <arma3cloud@gmail.com>
2023-11-24 16:49:17 -0700

    Basic .map importing into editor
    Thanks to RustMapper for his unity client code :)

---
+++ Assets/Scripts/Carbon/World/WorldConverter.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using static WorldSerialization;
+using ProtoBuf;
+using static Carbon.WorldManager;
+
+namespace Carbon
+{
+    public static class WorldConverter
+    {
+        public static float[,] ShortMapToFloatArray(TerrainMap<short> terrainMap)
+        {
+            float[,] array = new float[terrainMap.res, terrainMap.res];
+            int arrayLength = array.GetLength(0);
+            Parallel.For(0, arrayLength, i =>
+            {
+                for (int j = 0; j < arrayLength; j++)
+                    array[i, j] = BitUtility.Short2Float(terrainMap[i, j]);
+            });
+            return array;
+        }
+
+        public static MapInfo ConvertMaps(MapInfo terrains, TerrainMap<byte> splatMap, TerrainMap<byte> biomeMap, TerrainMap<byte> alphaMap)
+        {
+            terrains.splatMap = new float[splatMap.res, splatMap.res, 8];
+            terrains.biomeMap = new float[biomeMap.res, biomeMap.res, 4];
+            terrains.alphaMap = new bool[alphaMap.res, alphaMap.res];
+
+            var groundTask = Task.Run(() =>
+            {
+                Parallel.For(0, terrains.splatRes, i =>
+                {
+                    for (int j = 0; j < terrains.splatRes; j++)
+                        for (int k = 0; k < 8; k++)
+                            terrains.splatMap[i, j, k] = BitUtility.Byte2Float(splatMap[k, i, j]);
+                });
+            });
+
+            var biomeTask = Task.Run(() =>
+            {
+                Parallel.For(0, terrains.splatRes, i =>
+                {
+                    for (int j = 0; j < terrains.splatRes; j++)
+                        for (int k = 0; k < 4; k++)
+                            terrains.biomeMap[i, j, k] = BitUtility.Byte2Float(biomeMap[k, i, j]);
+                });
+            });
+
+            var alphaTask = Task.Run(() =>
+            {
+                Parallel.For(0, terrains.splatRes, i =>
+                {
+                    for (int j = 0; j < terrains.splatRes; j++)
+                    {
+                        if (alphaMap[0, i, j] > 0)
+                            terrains.alphaMap[i, j] = true;
+                        else
+                            terrains.alphaMap[i, j] = false;
+                    }
+                });
+            });
+            Task.WaitAll(groundTask, biomeTask, alphaTask);
+            return terrains;
+        }
+
+        public static MapInfo WorldToTerrain(WorldSerialization world)
+        {
+            MapInfo terrains = new MapInfo();
+            var terrainSize = new Vector3(world.world.size, 1000, world.world.size);
+            var terrainMap = new TerrainMap<short>(world.GetMap("terrain").data, 1);
+            var heightMap = new TerrainMap<short>(world.GetMap("height").data, 1);
+            var waterMap = new TerrainMap<short>(world.GetMap("water").data, 1);
+            var splatMap = new TerrainMap<byte>(world.GetMap("splat").data, 8);
+            var topologyMap = new TerrainMap<int>(world.GetMap("topology").data, 1);
+            var biomeMap = new TerrainMap<byte>(world.GetMap("biome").data, 4);
+            var alphaMap = new TerrainMap<byte>(world.GetMap("alpha").data, 1);
+
+            terrains.topology = topologyMap;
+            terrains.pathData = new PathData[0];
+            terrains.prefabData = new PrefabData[0];
+
+            terrains.terrainRes = heightMap.res;
+            terrains.splatRes = splatMap.res;
+            terrains.size = terrainSize;
+
+            var heightTask = Task.Run(() => ShortMapToFloatArray(heightMap));
+            var waterTask = Task.Run(() => ShortMapToFloatArray(waterMap));
+            terrains = ConvertMaps(terrains, splatMap, biomeMap, alphaMap);
+            Task.WaitAll(heightTask, waterTask);
+
+            terrains.land.heights = heightTask.Result;
+            terrains.water.heights = waterTask.Result;
+            return terrains;
+        }
+    }
+}
---
+++ Assets/Scripts/Carbon/World/WorldManager.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace Carbon
+{
+    [ExecuteAlways]
+    public class WorldManager : MonoBehaviour
+    {
+        #region Defines
+        public string filename = string.Empty;
+        public static Terrain Land { get; private set; }
+        public static Terrain Water { get; private set; }
+        public static Material WaterMaterial { get; private set; }
+        public static float[,,] Ground { get; private set; }
+        public static float[,,] Biome { get; private set; }
+        public static bool[,] Alpha { get; private set; }
+        public static bool AlphaDirty { get; set; } = true;
+        public static int SplatMapRes { get; private set; }
+        public static int HeightMapRes { get; private set; }
+        public static int AlphaMapRes { get => HeightMapRes - 1; }
+        public static LayerType CurrentLayerType { get; private set; }
+        private static TerrainLayer[] GroundLayers = null, BiomeLayers = null, TopologyLayers = null;
+        public static bool LayerDirty { get; private set; } = false;
+
+        public enum TerrainType
+        {
+            Land,
+            Water
+        }
+
+        public enum LayerType
+        {
+            Ground,
+            Biome,
+            Alpha,
+            Topology
+        }
+
+        public struct MapInfo
+        {
+            public int terrainRes;
+            public int splatRes;
+            public Vector3 size;
+            public float[,,] splatMap;
+            public float[,,] biomeMap;
+            public bool[,] alphaMap;
+            public TerrainInfo land;
+            public TerrainInfo water;
+            public TerrainMap<int> topology;
+            public PrefabData[] prefabData;
+            public PathData[] pathData;
+        }
+
+        public struct TerrainInfo
+        {
+            public float[,] heights;
+        }
+        #endregion
+
+        [InitializeOnLoadMethod]
+        private static void Init()
+        {
+            EditorApplication.update += OnProjectLoad;
+        }
+
+        private static void OnProjectLoad()
+        {
+            EditorApplication.update -= OnProjectLoad;
+            SetTerrainReferences();
+        }
+
+        public static void SetTerrainReferences() => Land = GameObject.FindGameObjectWithTag("Land").GetComponent<Terrain>();
+
+        private WorldSerialization world;
+        private PrefabLookup prefabs;
+
+        public WorldSerialization LoadWorld(string filename)
+        {
+            var blob = new WorldSerialization();
+            blob.Load(filename);
+            return blob;
+        }
+
+        public static void Load(MapInfo mapInfo, string path = "")
+        {
+            SetTerrains(mapInfo);
+            SetSplatMaps(mapInfo);
+            ClearSplatMapUndo();
+        }
+
+        private static void SetTerrains(MapInfo mapInfo)
+        {
+            HeightMapRes = mapInfo.terrainRes;
+
+            SetupTerrain(mapInfo, Water);
+            SetupTerrain(mapInfo, Land);
+        }
+
+        private static void SetupTerrain(MapInfo mapInfo, Terrain terrain)
+        {
+            Debug.Log(terrain.terrainData.size);
+            Debug.Log(mapInfo.size);
+
+            if (terrain.terrainData.size != mapInfo.size)
+            {
+                terrain.terrainData.heightmapResolution = mapInfo.terrainRes;
+                terrain.terrainData.size = mapInfo.size;
+                terrain.terrainData.alphamapResolution = mapInfo.splatRes;
+                terrain.terrainData.baseMapResolution = mapInfo.splatRes;
+            }
+
+            terrain.terrainData.SetHeights(0, 0, terrain.Equals(Land) ? mapInfo.land.heights : mapInfo.water.heights);
+        }
+
+        public static void SetSplatMap(float[,,] array, LayerType layer, int topology = -1)
+        {
+            if (array == null)
+            {
+                Debug.LogError($"SetSplatMap(array) is null.");
+                return;
+            }
+
+            if (layer == LayerType.Alpha)
+            {
+                Debug.LogWarning($"SetSplatMap(float[,,], {layer}) is not a valid layer to set. Use SetAlphaMap(bool[,]) to set {layer}.");
+                return;
+            }
+
+            // Check for array dimensions not matching alphamap.
+            if (array.GetLength(0) != SplatMapRes || array.GetLength(1) != SplatMapRes || array.GetLength(2) != LayerCount(layer))
+            {
+                Debug.LogError($"SetSplatMap(array[{array.GetLength(0)}, {array.GetLength(1)}, {LayerCount(layer)}]) dimensions invalid, should be " +
+                    $"array[{ SplatMapRes}, { SplatMapRes}, {LayerCount(layer)}].");
+                return;
+            }
+
+            switch (layer)
+            {
+                case LayerType.Ground:
+                    Ground = array;
+                    break;
+                case LayerType.Biome:
+                    Biome = array;
+                    break;
+            }
+
+            if (CurrentLayerType == layer)
+            {
+                if (!GetTerrainLayers().Equals(Land.terrainData.terrainLayers))
+                    Land.terrainData.terrainLayers = GetTerrainLayers();
+
+                RegisterSplatMapUndo($"{layer}");
+                Land.terrainData.SetAlphamaps(0, 0, array);
+                LayerDirty = false;
+            }
+        }
+
+        private static void SetSplatMaps(MapInfo mapInfo)
+        {
+            SplatMapRes = mapInfo.splatRes;
+            SetSplatMap(mapInfo.splatMap, LayerType.Ground);
+            SetSplatMap(mapInfo.biomeMap, LayerType.Biome);
+            SetAlphaMap(mapInfo.alphaMap);
+            Debug.Log("Loaded Splats");
+        }
+
+        public static void ClearSplatMapUndo()
+        {
+            foreach (var tex in Land.terrainData.alphamapTextures)
+                Undo.ClearUndo(tex);
+        }
+
+        public static void RegisterSplatMapUndo(string name) => Undo.RegisterCompleteObjectUndo(Land.terrainData.alphamapTextures, name);
+        public static void SetAlphaMap(bool[,] array)
+        {
+            if (array == null)
+            {
+                Debug.LogError($"SetAlphaMap(array) is null.");
+                return;
+            }
+
+            // Check for array dimensions not matching alphamap.
+            if (array.GetLength(0) != AlphaMapRes || array.GetLength(1) != AlphaMapRes)
+            {
+                // Special case for converting Alphamaps from the Rust resolution to the Unity Editor resolution.
+                if (array.GetLength(0) == SplatMapRes && array.GetLength(1) == SplatMapRes)
+                {
+                    if (Alpha == null || Alpha.GetLength(0) != AlphaMapRes)
+                        Alpha = new bool[AlphaMapRes, AlphaMapRes];
+
+                    Parallel.For(0, AlphaMapRes, i =>
+                    {
+                        for (int j = 0; j < AlphaMapRes; j++)
+                            Alpha[i, j] = array[i / 2, j / 2];
+                    });
+
+                    Land.terrainData.SetHoles(0, 0, Alpha);
+                    AlphaDirty = false;
+                    return;
+                }
+
+                else
+                {
+                    Debug.LogError($"SetAlphaMap(array[{array.GetLength(0)}, {array.GetLength(1)}]) dimensions invalid, should be array[{AlphaMapRes}, {AlphaMapRes}].");
+                    return;
+                }
+            }
+
+            Alpha = array;
+            Land.terrainData.SetHoles(0, 0, Alpha);
+            AlphaDirty = false;
+        }
+
+        public static TerrainLayer[] GetTerrainLayers()
+        {
+            if (GroundLayers == null || BiomeLayers == null || TopologyLayers == null)
+                SetTerrainLayers();
+
+            return CurrentLayerType switch
+            {
+                LayerType.Biome => BiomeLayers,
+                _ => GroundLayers
+            };
+        }
+
+        public static void SetTerrainLayers()
+        {
+            GroundLayers = GetGroundLayers();
+            BiomeLayers = GetBiomeLayers();
+            AssetDatabase.SaveAssets();
+        }
+
+        private static TerrainLayer[] GetGroundLayers()
+        {
+            TerrainLayer[] textures = new TerrainLayer[8];
+            textures[0] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Ground/Dirt.terrainlayer");
+            textures[0].diffuseTexture = Resources.Load<Texture2D>("Textures/Ground/dirt");
+            textures[1] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Ground/Snow.terrainlayer");
+            textures[1].diffuseTexture = Resources.Load<Texture2D>("Textures/Ground/snow");
+            textures[2] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Ground/Sand.terrainlayer");
+            textures[2].diffuseTexture = Resources.Load<Texture2D>("Textures/Ground/sand");
+            textures[3] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Ground/Rock.terrainlayer");
+            textures[3].diffuseTexture = Resources.Load<Texture2D>("Textures/Ground/rock");
+            textures[4] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Ground/Grass.terrainlayer");
+            textures[4].diffuseTexture = Resources.Load<Texture2D>("Textures/Ground/grass");
+            textures[5] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Ground/Forest.terrainlayer");
+            textures[5].diffuseTexture = Resources.Load<Texture2D>("Textures/Ground/forest");
+            textures[6] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Ground/Stones.terrainlayer");
+            textures[6].diffuseTexture = Resources.Load<Texture2D>("Textures/Ground/stones");
+            textures[7] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Ground/Gravel.terrainlayer");
+            textures[7].diffuseTexture = Resources.Load<Texture2D>("Textures/Ground/gravel");
+
+            return textures;
+        }
+
+        private static TerrainLayer[] GetBiomeLayers()
+        {
+            TerrainLayer[] textures = new TerrainLayer[4];
+            textures[0] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Biome/Arid.terrainlayer");
+            textures[0].diffuseTexture = Resources.Load<Texture2D>("Textures/Biome/arid");
+            textures[1] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Biome/Temperate.terrainlayer");
+            textures[1].diffuseTexture = Resources.Load<Texture2D>("Textures/Biome/temperate");
+            textures[2] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Biome/Tundra.terrainlayer");
+            textures[2].diffuseTexture = Resources.Load<Texture2D>("Textures/Biome/tundra");
+            textures[3] = AssetDatabase.LoadAssetAtPath<TerrainLayer>("Assets/Resources/Textures/Biome/Arctic.terrainlayer");
+            textures[3].diffuseTexture = Resources.Load<Texture2D>("Textures/Biome/arctic");
+            return textures;
+        }
+
+        public static int LayerCount(LayerType layer)
+        {
+            return layer switch
+            {
+                LayerType.Ground => 8,
+                LayerType.Biome => 4,
+                _ => 2
+            };
+        }
+
+        /*
+        private void SpawnPrefabs(WorldSerialization blob)
+        {
+            foreach (var prefab in blob.world.prefabs)
+            {
+                var prefabGameObject = RustAssetProcessor.Prefabs[prefab.];
+                if (!prefabGameObject) continue;
+
+                var instanceGameObject = Instantiate(prefabGameObject, prefab.position, prefab.rotation);
+                if (!instanceGameObject) continue;
+
+                instanceGameObject.transform.localScale = prefab.scale;
+                instanceGameObject.SetActive(true);
+            }
+        }
+
+        */
+    }
+}
